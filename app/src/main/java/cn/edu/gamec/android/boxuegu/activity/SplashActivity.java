660fe603b24d8b0d070acf51b46c073b1058de82
liyinliang <1462778722@qq.com>
2017-12-26 10:40:51 +0800

    第二天

---
+++ app/src/main/java/cn/edu/gamec/android/boxuegu/activity/SplashActivity.java
@@ -0,0 +1,15 @@
+package cn.edu.gamec.android.boxuegu.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.edu.gamec.android.boxuegu.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
