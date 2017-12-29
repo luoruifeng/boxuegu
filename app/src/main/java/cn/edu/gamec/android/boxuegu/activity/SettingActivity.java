f4f6f622c13c61dfd3ce335d6b116ee86853d660
liyinliang <1462778722@qq.com>
2017-12-29 11:57:41 +0800

    第五天

--- app/src/main/java/cn/edu/gamec/android/boxuegu/activity/SettingActivity.java
+++ app/src/main/java/cn/edu/gamec/android/boxuegu/activity/SettingActivity.java
@@ -1,8 +1,11 @@
 package cn.edu.gamec.android.boxuegu.activity;
 
+import java.util.zip.Deflater;
+
 /**
  * Created by student on 17/12/27.
  */
 
 public class SettingActivity {
+    public static Deflater instance;
 }
